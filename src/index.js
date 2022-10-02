fa54c4c8ec86f2724d8196a6fade6eb129658ca8
debug-jay <jaydingulley4@gmail.com>
2022-10-02 03:36:22 -0500

    a

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {CookiesProvider} from "react-cookie";
+import { RoutesPages } from './pages/RoutesPages';
+
 
 export default function App() {
   return (
