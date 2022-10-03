a245517a03315d825f9748a009bd47142106ba05
debug-jay <jaydingulley4@gmail.com>
2022-10-02 21:31:54 -0500

    aaa

--- functions/backend.js
+++ functions/backend.js
@@ -1,10 +1,23 @@
+const fetch = require('node-fetch');
 
+const API_ENDPOINT = 'https://ec2-34-201-91-232.compute-1.amazonaws.com:5001/api/test';
 
 exports.handler = async (event, context) => {
     
+    let response
+    try{
+        response = await fetch(API_ENDPOINT)
+    } catch(err){
+        return {
+            statusCode: err.statusCode || 500,
+            body: JSON.stringify({
+                error: err.message
+            })
+        }
+    }
    
     return{
         statusCode: 200,
-        body: JSON.stringify(event.body)
+        body: JSON.stringify({data: response})
     }
 }
