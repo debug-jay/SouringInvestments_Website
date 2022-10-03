7f61416958685bb50588975fa18eb8a08a152177
debug-jay <jaydingulley4@gmail.com>
2022-10-03 15:36:10 -0500

    a

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -16,10 +16,10 @@ export class SignUpPage extends PureComponent {
             c_password: '',
             privatekey: '',
 
-            emailCheck: false,
-            usernameCheck: false,
-            passCheck: false,
-            secondpassCheck: false
+            emailCheck: new Boolean,
+            usernameCheck: new Boolean,
+            passCheck: new Boolean,
+            secondpassCheck: new Boolean
         }
 
         this.textGreen = "text-green-400 text-xs mt-1";
