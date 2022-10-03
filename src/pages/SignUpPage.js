1f72ec42feeab1292ff9f8888487f2ed5dea2424
debug-jay <jaydingulley4@gmail.com>
2022-10-03 02:50:28 -0500

    a

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -146,7 +146,7 @@ export class SignUpPage extends PureComponent {
 
         try{
             console.log("running");
-             await axios.post('https://bdrfef3yols5u2hpsujl7542xa0jhxvn.lambda-url.us-east-1.on.aws/api/userSignUp',{
+             await axios.post('https://bdrfef3yols5u2hpsujl7542xa0jhxvn.lambda-url.us-east-1.on.aws/api/testPost',{
               firstname: this.state.firstname,
               lastname: this.state.lastname,
               email: this.state.email
